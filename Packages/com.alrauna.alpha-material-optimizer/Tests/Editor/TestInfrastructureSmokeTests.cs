035a2efb20136ad13787a37e81a60162f3f50596
Alrauna <67886220+Alrauna@users.noreply.github.com>
2026-08-14 23:00:22 -0400

    Set up NDMF development test environment

---
+++ Packages/com.alrauna.alpha-material-optimizer/Tests/Editor/TestInfrastructureSmokeTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Alrauna.AlphaMaterialOptimizer.Tests.Editor
+{
+    public sealed class TestInfrastructureSmokeTests
+    {
+        [Test]
+        public void PackageEditorTestAssemblyIsExecutable()
+        {
+            Assert.Pass();
+        }
+    }
+}
