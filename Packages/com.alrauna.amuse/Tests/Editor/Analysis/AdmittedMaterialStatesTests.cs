4c208a65f9a1934889eafb926261267dc8151f22
Alrauna <67886220+Alrauna@users.noreply.github.com>
2026-08-24 10:44:37 -0400

    feat: admit colour and vector properties under the singleton rule

--- Packages/com.alrauna.amuse/Tests/Editor/Analysis/AdmittedMaterialStatesTests.cs
+++ Packages/com.alrauna.amuse/Tests/Editor/Analysis/AdmittedMaterialStatesTests.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using Alrauna.Amuse.Editor.Analysis;
 using Alrauna.Amuse.Editor.Host;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace Alrauna.Amuse.Tests.Editor.Analysis
 {
@@ -17,6 +19,22 @@ namespace Alrauna.Amuse.Tests.Editor.Analysis
                 "material._Cutoff",
                 finiteExact,
                 values);
+        }
+
+        private static IReadOnlyDictionary<
+            int, IReadOnlyList<CapturedFloatBinding>> Components(
+            params (int Index, float Value)[] entries)
+        {
+            var components = new Dictionary<
+                int, IReadOnlyList<CapturedFloatBinding>>();
+            foreach (var entry in entries)
+            {
+                components.Add(
+                    entry.Index,
+                    new[] { Binding(true, entry.Value) });
+            }
+
+            return components;
         }
 
         [Test]
@@ -127,6 +145,148 @@ namespace Alrauna.Amuse.Tests.Editor.Analysis
                     float.NaN,
                     out _),
                 Is.EqualTo(AdmittedPropertyOutcome.SourcesDisagree));
+        }
+
+        [Test]
+        public void AnimatedAlphaReassertsItsDefaultAndPreservesTheWholeColor()
+        {
+            var serialized = new Color(0.1f, 0.2f, 0.3f, 0.4f);
+
+            var outcome = AdmittedMaterialStates.AdmitColor(
+                Components((3, 0.4f)), serialized, out var admitted);
+
+            Assert.That(outcome, Is.EqualTo(AdmittedPropertyOutcome.Singleton));
+            Assert.That(admitted, Is.EqualTo(serialized));
+        }
+
+        [Test]
+        public void AnimatedAlphaDoesNotOverrideADifferentDefault()
+        {
+            Assert.That(
+                AdmittedMaterialStates.AdmitColor(
+                    Components((3, 1f)),
+                    new Color(0.1f, 0.2f, 0.3f, 0.4f),
+                    out _),
+                Is.EqualTo(AdmittedPropertyOutcome.SourcesDisagree));
+        }
+
+        [Test]
+        public void DisagreeingColorComponentSourcesRefuseTheWholeColor()
+        {
+            var components = new Dictionary<
+                int, IReadOnlyList<CapturedFloatBinding>>
+            {
+                [3] = new[] { Binding(true, 0.4f), Binding(true, 1f) },
+            };
+
+            Assert.That(
+                AdmittedMaterialStates.AdmitColor(
+                    components,
+                    new Color(0.1f, 0.2f, 0.3f, 0.4f),
+                    out _),
+                Is.EqualTo(AdmittedPropertyOutcome.SourcesDisagree));
+        }
+
+        [Test]
+        public void ComponentThreePreservesUnanimatedRgbComponents()
+        {
+            var outcome = AdmittedMaterialStates.AdmitColor(
+                Components((3, 1f)),
+                new Color(0.25f, 0.5f, 0.75f, 1f),
+                out var admitted);
+
+            Assert.That(outcome, Is.EqualTo(AdmittedPropertyOutcome.Singleton));
+            Assert.That(
+                admitted,
+                Is.EqualTo(new Color(0.25f, 0.5f, 0.75f, 1f)));
+        }
+
+        [Test]
+        public void AnimatedVectorComponentsReassertTheirOwnDefaults()
+        {
+            var serialized = new Vector4(1f, 1f, 0f, 0f);
+
+            var outcome = AdmittedMaterialStates.AdmitVector(
+                Components((0, 1f), (1, 1f)),
+                serialized,
+                out var admitted);
+
+            Assert.That(outcome, Is.EqualTo(AdmittedPropertyOutcome.Singleton));
+            Assert.That(admitted, Is.EqualTo(serialized));
+        }
+
+        [Test]
+        public void AnimatedVectorComponentDoesNotOverrideADifferentDefault()
+        {
+            Assert.That(
+                AdmittedMaterialStates.AdmitVector(
+                    Components((0, 2f)),
+                    new Vector4(1f, 1f, 0f, 0f),
+                    out _),
+                Is.EqualTo(AdmittedPropertyOutcome.SourcesDisagree));
+        }
+
+        [Test]
+        public void NonFiniteExactVectorComponentRefuses()
+        {
+            var components = new Dictionary<
+                int, IReadOnlyList<CapturedFloatBinding>>
+            {
+                [0] = new[] { Binding(false, 1f) },
+            };
+
+            Assert.That(
+                AdmittedMaterialStates.AdmitVector(
+                    components, new Vector4(1f, 2f, 3f, 4f), out _),
+                Is.EqualTo(AdmittedPropertyOutcome.NotFiniteExact));
+        }
+
+        [Test]
+        public void UnanimatedVectorComponentsPreserveSerializedValues()
+        {
+            var outcome = AdmittedMaterialStates.AdmitVector(
+                Components((0, 1f)),
+                new Vector4(1f, 2f, 3f, 4f),
+                out var admitted);
+
+            Assert.That(outcome, Is.EqualTo(AdmittedPropertyOutcome.Singleton));
+            Assert.That(admitted, Is.EqualTo(new Vector4(1f, 2f, 3f, 4f)));
+        }
+
+        [TestCase(-1)]
+        [TestCase(4)]
+        public void InvalidComponentIndexIsAProgrammingDefect(int component)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                AdmittedMaterialStates.AdmitVector(
+                    Components((component, 1f)), Vector4.one, out _));
+        }
+
+        [Test]
+        public void NonFiniteExactPrecedesDisagreementRegardlessOfMapOrder()
+        {
+            var disagreementFirst = new Dictionary<
+                int, IReadOnlyList<CapturedFloatBinding>>
+            {
+                [0] = new[] { Binding(true, 2f) },
+                [3] = new[] { Binding(false, 4f) },
+            };
+            var nonFiniteFirst = new Dictionary<
+                int, IReadOnlyList<CapturedFloatBinding>>
+            {
+                [3] = new[] { Binding(false, 4f) },
+                [0] = new[] { Binding(true, 2f) },
+            };
+            var serialized = new Vector4(1f, 2f, 3f, 4f);
+
+            Assert.That(
+                AdmittedMaterialStates.AdmitVector(
+                    disagreementFirst, serialized, out _),
+                Is.EqualTo(AdmittedPropertyOutcome.NotFiniteExact));
+            Assert.That(
+                AdmittedMaterialStates.AdmitVector(
+                    nonFiniteFirst, serialized, out _),
+                Is.EqualTo(AdmittedPropertyOutcome.NotFiniteExact));
         }
     }
 }
